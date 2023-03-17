d816ae8c059e7959057c8ae93886babfb1545093
Caroline Mandl <cc221024@fhstp.ac.at>
2023-03-17 22:10:47 +0100

    Everything we did in the third lesson

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
