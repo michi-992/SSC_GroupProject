8e3c72d0056cf3d3e616027f7d80d9f7f2b0c349
noell <cc221014@fhstp.ac.at>
2023-03-17 16:45:23 +0100

    everything we did in class

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello from the index.js router. Nice ta meetcha.');
+});
+
+router.get('/example/b', function (req, res, next) {
+    console.log('I am the first function. I do nothing but talk.');
+    next();
+}, function (req, res) {
+    res.send('This is the response from B!')
+});
+
+const cbC1 = function (req, res, next) {
+    console.log('cbC1');
+    next();
+};
+
+const cbC2 = function (req, res, next) {
+    console.log('cbC2');
+};
+
+const cbC3 = function (req, res) {
+    res.send('Hello from C! (cbC3)')
+};
+
+router.get('/example/c', [cbC1, cbC2, cbC3]);
+
+const cbD1 = function (req, res, next) {
+    console.log('cbD1');
+    next();
+};
+
+const cbD2 = function (req, res, next) {
+    console.log('cbD2');
+    next();
+};
+
+router.get('/example/d', [cbD1, cbD2], function (req, res, next) {
+    console.log('Hello from this function. Response will be sent by next one.');
+    next();
+}, function cbD3 (req, res) {
+    res.send('This is the response from cbD3.');
+});
+
+router.post('/', (req, res, next) => {
+    console.log(req.body);
+    res.send('Received a POST request.')
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello from the users.js router.');
+});
+
+router.get('/:id', (req, res, next) => {
+    res.send('Received a GET request for user with id: ' + req.params.id);
+});
+
+module.exports = router;
